1174420757b0c5dd79cd47854ee4e0f0105db50f
Benoit Zhong <benoit.zhong@gmail.com>
2018-11-20 21:48:28 +0100

    Fixed slot type

--- action-app_carburator.py
+++ action-app_carburator.py
@@ -6,6 +6,7 @@ from hermes_python.hermes import Hermes
 from hermes_python.ontology import *
 import io
 import requests
+import json
 
 CONFIG_INI = "config.ini"
 CARBURATOR_API_URL = "http://carburator.fr/api/"
@@ -34,6 +35,7 @@ class Carburator(object):
 
     # --> Sub callback function, one per intent
     def askPrice_callback(self, hermes, intent_message):
+        print(intent_message)
         # terminate the session first if not continue
         hermes.publish_end_session(intent_message.session_id, "")
 
@@ -44,6 +46,7 @@ class Carburator(object):
         latitude = self.config.get("secret").get("latitude")
         if longitude is not None and latitude is not None:
             station_data = requests.get(CARBURATOR_API_URL+'stations/lon/'+str(longitude)+'/lat/'+str(latitude)+'?limit=1').json()
+            #json.loads('[{"_id":"5be07e22074157507f5ab26e","address":"145, Rue Anatole France","city":"VILLEURBANNE","id":"69100001","last_modified":"2018-11-20T20:30:32.956Z","pop":"R","postcode":"69100","services":["Toilettes publiques","Boutique alimentaire","Vente de gaz domestique (Butane, Propane)","Automate CB"],"prices":{"1":{"value":"1.417","updated":"2018-11-20T10:04:05.000Z","name":"Gazole"},"2":{"value":"1.397","updated":"2018-11-20T10:04:05.000Z","name":"SP95"},"6":{"value":"1.428","updated":"2018-11-20T10:04:06.000Z","name":"SP98"}},"lonlat":[4.884,45.769]}]')
         else:
             hermes.publish_start_session_notification(intent_message.site_id, "Error: no location was specified.", "")
             return
@@ -51,8 +54,8 @@ class Carburator(object):
         # action code goes here...
         oil_types = ['gasoline','diesel']
         oil_type = None
-        if intent_message.slots.oil_type:
-            oil_type = intent_message.slots.oil_type.first().value
+        if intent_message.slots.oilType:
+            oil_type = intent_message.slots.oilType.first().value
             # check if the category is valide
             if oil_type.encode("utf-8") not in oil_types:
                 oil_type = None
@@ -76,7 +79,8 @@ class Carburator(object):
     # --> Master callback function, triggered everytime an intent is recognized
     def master_intent_callback(self,hermes, intent_message):
         coming_intent = intent_message.intent.intent_name
-        if coming_intent == 'askPrice':
+        print(coming_intent)
+        if 'askPrice' in coming_intent:
             self.askPrice_callback(hermes, intent_message)
         # more callback and if condition goes here...
 
