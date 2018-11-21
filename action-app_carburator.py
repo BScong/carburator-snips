74c42f1b0224ea16220153b2ab0be3aa9e314e32
Benoit Zhong <benoit.zhong@gmail.com>
2018-11-21 12:48:45 +0100

    Refactor askPrice and implemented setOiltype and setCity

--- action-app_carburator.py
+++ action-app_carburator.py
@@ -6,7 +6,7 @@ from hermes_python.hermes import Hermes
 from hermes_python.ontology import *
 import io
 import requests
-import json
+import googlemaps
 
 CONFIG_INI = "config.ini"
 CARBURATOR_API_URL = "http://carburator.fr/api/"
@@ -18,6 +18,16 @@ MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
+def getLonLat(gmaps, city):
+    try:
+        if gmaps:
+            geocode_result = gmaps.geocode(city)
+            return geocode_result
+        else:
+            raise Error('No API key specified');
+    except Exception e:
+        raise e
+
 class Carburator(object):
     """
     Class used to wrap action code with mqtt connection
@@ -27,43 +37,25 @@ class Carburator(object):
         # get the configuration if needed
         try:
             self.config = SnipsConfigParser.read_configuration_file(CONFIG_INI)
+            self.gmaps = None
+            try:
+                if self.config.get("secret").get("google_maps_API_key"):
+                    self.gmaps = googlemaps.Client(key=self.config.get("secret").get("google_maps_API_key"))
+            except:
+                self.gmaps = None
         except :
             self.config = None
+        self.longitude = 0
+        self.latitude = 0
+        self.city = ''
+        self.oil_type = ''
 
         # start listening to MQTT
         self.start_blocking()
 
+
     # --> Sub callback function, one per intent
     def setOilType_callback(self, hermes, intent_message):
-        return
-        
-    def getServices_callback(self, hermes, intent_message):
-        return
-
-    def getStationAddress_callback(self, hermes, intent_message):
-        return
-
-    def setCity_callback(self, hermes, intent_message):
-        return
-
-    def askPrice_callback(self, hermes, intent_message):
-        print(intent_message)
-        # terminate the session first if not continue
-        hermes.publish_end_session(intent_message.session_id, "")
-
-        # action code goes here...
-        print('[Received] intent: '+ str(intent_message.intent.intent_name))
-
-        longitude = self.config.get("secret").get("longitude")
-        latitude = self.config.get("secret").get("latitude")
-        if longitude is not None and latitude is not None:
-            station_data = requests.get(CARBURATOR_API_URL+'stations/lon/'+str(longitude)+'/lat/'+str(latitude)+'?limit=1').json()
-            #json.loads('[{"_id":"5be07e22074157507f5ab26e","address":"145, Rue Anatole France","city":"VILLEURBANNE","id":"69100001","last_modified":"2018-11-20T20:30:32.956Z","pop":"R","postcode":"69100","services":["Toilettes publiques","Boutique alimentaire","Vente de gaz domestique (Butane, Propane)","Automate CB"],"prices":{"1":{"value":"1.417","updated":"2018-11-20T10:04:05.000Z","name":"Gazole"},"2":{"value":"1.397","updated":"2018-11-20T10:04:05.000Z","name":"SP95"},"6":{"value":"1.428","updated":"2018-11-20T10:04:06.000Z","name":"SP98"}},"lonlat":[4.884,45.769]}]')
-        else:
-            hermes.publish_start_session_notification(intent_message.site_id, "Error: no location was specified.", "")
-            return
-
-        # action code goes here...
         oil_types = ['gasoline','diesel']
         oil_type = None
         if intent_message.slots.oilType:
@@ -71,27 +63,90 @@ class Carburator(object):
             # check if the category is valide
             if oil_type.encode("utf-8") not in oil_types:
                 oil_type = None
+            else:
+                self.oil_type = oil_type.encode("utf-8")
 
         if oil_type is None:
             hermes.publish_start_session_notification(intent_message.site_id, "Error: no oil type was specified.", "")
             return
 
+    def setCity_callback(self, hermes, intent_message):
+        if intent_message.slots.city:
+            try:
+                city = intent_message.slots.city.first().value.encode("utf-8")
+                lon, lat = getLonLat(self.gmaps, city)
+                self.longitude = lon
+                self.latitude = lat
+                self.city = city
+            except:
+                hermes.publish_start_session_notification(intent_message.site_id, "Error finding location", "")
+                return
+
+        return
+
+    def getServices_callback(self, hermes, intent_message):
+        return
+
+    def getStationAddress_callback(self, hermes, intent_message):
+        return
+
+
+
+    def askPrice_callback(self, hermes, intent_message):
+        # terminate the session first if not continue
+        hermes.publish_end_session(intent_message.session_id, "")
+
+        # Find oil type
+        oil_type = None
+        if intent_message.slots.oilType:
+            oil_types = ['gasoline','diesel']
+            oil_type = intent_message.slots.oilType.first().value
+            # check if the category is valide
+            if oil_type.encode("utf-8") not in oil_types:
+                oil_type = None
+        elif self.oil_type != '':
+            oil_type = self.oil_type
+        else:
+            ## trigger setOilType intent
+            continue
+
+        # Find location
+        lon, lat = 0, 0
+        if intent_message.slots.city:
+            try:
+                city = intent_message.slots.city.first().value.encode("utf-8")
+                lon, lat = getLonLat(self.gmaps, city)
+            except:
+                hermes.publish_start_session_notification(intent_message.site_id, "Error finding location", "")
+                return
+        elif self.city != '':
+            lon, lat = self.longitude, self.latitude
+        else:
+            ## trigger setCity intent
+            continue
+
         try:
-            prices = station_data[0]['prices']
-            if oil_type == 'gasoline':
-                price = prices['2']['value']
-            else:
-                price = price['1']['value']
-        except Exception as error:
-            hermes.publish_start_session_notification(intent_message.site_id, "Error: impossible to find gas price.", "")
+            station_data = requests.get(CARBURATOR_API_URL+'stations/lon/'+str(longitude)+'/lat/'+str(latitude)+'?limit=1').json()
+            try:
+                prices = station_data[0]['prices']
+                if oil_type == 'gasoline':
+                    price = prices['2']['value']
+                else:
+                    price = price['1']['value']
+            except Exception as error:
+                hermes.publish_start_session_notification(intent_message.site_id, "Error: impossible to find gas price.", "")
+                return
+        except:
+            hermes.publish_start_session_notification(intent_message.site_id, "Error when fetching gas data.", "")
             return
+
         hermes.publish_start_session_notification(intent_message.site_id, "The " + str(oil_type) + ' is currently at ' + price + ' euros per liter.', "")
 
 
     # --> Master callback function, triggered everytime an intent is recognized
     def master_intent_callback(self,hermes, intent_message):
         coming_intent = intent_message.intent.intent_name
-        print(coming_intent)
+        print('[Received] intent: '+ str(intent_message.intent.intent_name))
         if 'askPrice' in coming_intent:
             self.askPrice_callback(hermes, intent_message)
         elif 'setOilType' in coming_intent:
